505d2d8b53fb00d34b009f333fa45b96f59b9b2c
Vladislav <necessarydp@gmail.com>
2020-07-23 14:06:32 +0300

    Updates

---
+++ precache-manifest.6cfb47ef377d3c1facf30d1cf2da7185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c329bc3fcab61c41a56e379b99ecb4e",
+    "url": "/ruchko-naranga/index.html"
+  },
+  {
+    "revision": "7a9c8c5e17f02f4a903b",
+    "url": "/ruchko-naranga/static/css/2.fab5e3fc.chunk.css"
+  },
+  {
+    "revision": "7a9c8c5e17f02f4a903b",
+    "url": "/ruchko-naranga/static/js/2.052806ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ruchko-naranga/static/js/2.052806ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e0315f3e67b50c9ba7",
+    "url": "/ruchko-naranga/static/js/main.2807b695.chunk.js"
+  },
+  {
+    "revision": "036b4e437547d3c477a7",
+    "url": "/ruchko-naranga/static/js/runtime-main.5cd3744d.js"
+  }
+]);
